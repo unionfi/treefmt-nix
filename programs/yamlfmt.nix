098c47ebeac64d15f8cb812e3aff51095fbdd4e0
benluelo <git@luelo.dev>
2024-03-13 00:21:44 -0400

    feat: make yamlfmt more configurable

--- programs/yamlfmt.nix
+++ programs/yamlfmt.nix
@@ -1,17 +1,31 @@
 { lib, pkgs, config, ... }:
 let
   cfg = config.programs.yamlfmt;
+  yamlExts = [ "*.yaml" "*.yml" ];
 in
 {
   options.programs.yamlfmt = {
     enable = lib.mkEnableOption "yamlfmt";
     package = lib.mkPackageOption pkgs "yamlfmt" { };
+    includes = lib.mkOption {
+      description = "Path / file patterns to include for yamlfmt";
+      type = lib.types.listOf lib.types.str;
+      example = yamlExts;
+      default = yamlExts;
+    };
+
+    excludes = lib.mkOption {
+      description = "Path / file patterns to exclude for yamlfmt";
+      type = lib.types.listOf lib.types.str;
+      example = [ "**/vendor/**" ];
+      default = [ ];
+    };
   };
 
   config = lib.mkIf cfg.enable {
     settings.formatter.yamlfmt = {
+      inherit (cfg) includes excludes;
       command = cfg.package;
-      includes = [ "*.yaml" "*.yml" ];
     };
   };
 }
